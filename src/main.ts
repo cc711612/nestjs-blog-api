85e4dd46886753e21d572ecbe32cea2e7a7caebd
Roy <cc711612@gmail.com>
2025-03-09 22:27:43 +0800

    env get port

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
