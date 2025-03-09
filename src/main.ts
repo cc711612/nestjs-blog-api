666a1ee795ee1802e4b37ad13c9740997cb3c351
Roy <cc711612@gmail.com>
2025-03-09 23:14:02 +0800

    調整 port

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT ? process.env.PORT : 3000;
   await app.listen(port);
 }
 bootstrap();
